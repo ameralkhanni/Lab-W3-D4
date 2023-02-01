6d7cf56d3950f450b27176312af92ab9944b3b51
Amer <ameralkhanni@gmail.com>
2023-02-01 15:05:40 +0300

    HW-W3-D4

---
+++ Lab-W3-D4/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Nav from './comp/Nav'
+import Home from './comp/Home'
+import Blog from './comp/Blog'
+import Blog2 from './comp/Blog2'
+import Blog3 from './comp/Blog3'
+import Blog4 from './comp/Blog4'
+import Footer from './comp/Footer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Nav/>
+      <Home/>
+      <Blog/>
+      <Blog2/>
+      <Blog3/>
+      <Blog4/>
+      <Footer/>
+
+
+
+
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ Lab-W3-D4/src/comp/Blog4.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Box, Flex, Link, chakra } from "@chakra-ui/react";
+
+export default function Blog4(){
+  return (
+    <Flex
+      bg="#edf3f8"
+      _dark={{ bg: "#3e3e3e" }}
+      p={50}
+      w="full"
+      alignItems="center"
+      justifyContent="right"
+    >
+      <Box
+        bg="white"
+        _dark={{ bg: "gray.800" }}
+        mx={{ lg: 8 }}
+        display={{ lg: "flex" }}
+        maxW={{ lg: "5xl" }}
+        shadow={{ lg: "lg" }}
+        rounded={{ lg: "lg" }}
+
+      >
+        <Box w={{ lg: "50%" }}>
+          <Box
+            h={{ base: 64, lg: "full" }}
+            rounded={{ lg: "lg" }}
+            bgSize="cover"
+          
+
+
+            style={{
+              backgroundImage:
+                "url('https://plus.unsplash.com/premium_photo-1661700225464-dbc1a1aa21ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')",
+            }}
+          ></Box>
+        </Box>
+
+        <Box py={12} px={6} maxW={{ base: "xl", lg: "5xl" }} w={{ lg: "50%" }}>
+          <chakra.h2
+            fontSize={{ base: "2xl", md: "3xl" }}
+            color="gray.800"
+            _dark={{ color: "white" }}
+            fontWeight="bold"
+          >
+            Build Your New{" "}
+            <chakra.span color="brand.600" _dark={{ color: "brand.400" }}>
+              Idea
+            </chakra.span>
+          </chakra.h2>
+          <chakra.p mt={4} color="gray.600" _dark={{ color: "gray.400" }}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            modi reprehenderit vitae exercitationem aliquid dolores ullam
+            temporibus enim expedita aperiam mollitia iure consectetur dicta
+            tenetur, porro consequuntur saepe accusantium consequatur.
+          </chakra.p>
+
+          <Box mt={8}>
+            <Link
+              bg="gray.900"
+              color="gray.100"
+              px={5}
+              py={3}
+              fontWeight="semibold"
+              rounded="lg"
+              _hover={{ bg: "gray.800" }}
+            >
+              Learn More
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </Flex>
+    
+    
+  );
+};
+
